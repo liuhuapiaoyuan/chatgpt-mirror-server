f50cbf558382ada2aa548216911cb2089c732ae0
abc <guohl1990@126.com>
2023-12-02 11:50:35 +0800

    bugfix(backend-api): fixed upload file CDN url

--- backend-api/backend-api.go
+++ backend-api/backend-api.go
@@ -84,12 +84,13 @@ func ProxyAll(r *ghttp.Request) {
 		// 如果path 以 ‘backend-api/files’ startwith开头
 		isDownload := strings.HasPrefix(path, "/backend-api/files") && strings.HasSuffix(path, "download")
 		isGizmosInfo := strings.HasPrefix(path, "/backend-api/gizmos/")
+		isUpdateFile := strings.HasPrefix(path, "/backend-api/files") && r.Request.Method == "POST"
 		isCreateConversation := strings.HasPrefix(path, "/backend-api/conversation/gen_title")
 		if isCreateConversation {
 			CreateConversation(ctx, userId, accessToken, r.UserAgent(), path)
 		}
 
-		if isDownload || isGizmosInfo {
+		if isDownload || isUpdateFile || isGizmosInfo {
 			g.Log().Info(ctx, "path", path)
 			g.Log().Info(ctx, "path content-type",
 				response.Header.Get("Content-Type"))
