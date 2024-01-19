fbd43e1f59649794dc2f9b6d9111d581da0cb8f8
liuhuapiaoyuan <278780765@qq.com>
2024-01-19 16:08:58 +0800

    fix: 修改一些细节

--- main.go
+++ main.go
@@ -22,7 +22,6 @@ func main() {
 	cmd.Main.Run(gctx.New())
 
 	// 打印一段话： 服务已经启动，请访问 http://127.0.0.1
-	fmt.Println("服务已经启动，请访问 http://127.0.0.1")
-
+	//fmt.Println("服务已经启动，请访问 http://127.0.0.1")
 
 }
